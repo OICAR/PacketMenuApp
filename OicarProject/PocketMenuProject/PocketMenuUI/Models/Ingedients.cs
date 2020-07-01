3ad1774481ba0492a917946ca74269cfe4036abf
nsricaz <nsricaz@racunarstvo.hr>
2020-07-01 11:40:23 +0200

    dev Caterer and meals entity

---
+++ OicarProject/PocketMenuProject/PocketMenuUI/Models/Ingedients.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PocketMenuUI.Models
+{
+
+    enum MesureUnit {
+
+
+        l,
+        mg,
+        dl,
+        cl
+
+
+
+    }
+
+
+    public class Ingedients
+    {
+
+        public string IngredientName { get; set; }
+
+        public MesureUnit Unit { get; set; }
+
+        public double Amount { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}

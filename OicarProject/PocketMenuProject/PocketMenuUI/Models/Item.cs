c91c082b0f8ff0f7623e7c1cf3060a6847628deb
jgudec <jakov.gudec@gmail.com>
2020-07-19 22:10:54 +0200

    Populate Menu View

--- OicarProject/PocketMenuProject/PocketMenuUI/Models/Item.cs
+++ OicarProject/PocketMenuProject/PocketMenuUI/Models/Item.cs
@@ -7,13 +7,28 @@ namespace PocketMenuUI.Models
 {
     public class Item
     {
+        public Item()
+        {
+        }
+
+        public Item(string title, double price)
+        {
+            Title = title;
+            Price = price;
+        }
+
+        public Item(string title, List<Ingredients> ingredients, double price)
+        {
+            Title = title;
+            Ingredients = ingredients;
+            Price = price;
+        }
+
         public int ID { get; set; }
+        public int CatererFacilityID { get; set; }
+        public List<Ingredients> Ingredients { get; set; }
 
         public string Title { get; set; }
-
-        public int CatererFacilityID { get; set; }
-
-        public List<Ingredients> Ingredients { get; set; }
 
         public double Price { get; set; }
 
